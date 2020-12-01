e85f2cd75ba9c31109b0972694c8ca842bc80084
SAMANU <manuel.santos@mercedes-benz.io>
2020-12-01 17:00:54 +0000

    Delete to fix author name

--- kubernetes-curl-job-deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Create config for kubernetes-curl-job.sh script
-kubectl create configmap kubernetes-curl-job-script --from-file=kubernetes-curl-job.sh
-
-# Apply Job
-kubectl apply -f kubernetes-curl-job.yaml
